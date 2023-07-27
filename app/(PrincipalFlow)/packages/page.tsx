438bc7fa97eb2cef99084a667360afb5e4af6b56
julianbesteiro <julianbesteiro1@gmail.com>
2023-07-27 14:35:43 -0300

    fix: edit dropdown card arrow on desktop so it is placed at the right side of the card and edit the arrow in the general card so it is placed in the middle of the circle

--- app/(PrincipalFlow)/packages/page.tsx
+++ app/(PrincipalFlow)/packages/page.tsx
@@ -1,25 +1,7 @@
 import React, { FC } from 'react';
-import { DropdownCard } from '../../../components/ui/dropdownCard/DropdownCard';
-import { GeneralCard } from '../../../components/ui/generalCard/GeneralCard';
 
 const InitWorkDay: FC = () => {
-    return (
-        <>
-            <DropdownCard title={'title'} subtitle={'subtitle'}>
-                <li className='flex'>shipment 6</li>
-                <li className='flex'>shipment 7</li>
-                <li className='flex'>shipment 8</li>
-                <li className='flex'>shipment 9</li>
-                <li className='flex'>shipment 10</li>
-            </DropdownCard>
-            <GeneralCard title={'title'}>
-                <p className='text-gray-800 text-base'>TEST</p>
-                <p className='text-gray-800 text-base'>TEST</p>
-                <p className='text-gray-800 text-base'>TEST</p>
-                <p className='text-gray-800 text-base'>TEST</p>
-            </GeneralCard>
-        </>
-    );
+    return <></>;
 };
 
 export default InitWorkDay;
