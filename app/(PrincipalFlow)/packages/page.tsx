e4ed686b42cc2cb378d453b8d04d23f1854aa519
julianbesteiro <julianbesteiro1@gmail.com>
2023-07-27 13:10:01 -0300

    fix: set up cards width for desktop and dynamic titles passed as props for cards

--- app/(PrincipalFlow)/packages/page.tsx
+++ app/(PrincipalFlow)/packages/page.tsx
@@ -1,9 +1,23 @@
 import React, { FC } from 'react';
+import { DropdownCard } from '../../../components/ui/dropdownCard/DropdownCard';
+import { GeneralCard } from '../../../components/ui/generalCard/GeneralCard';
 
 const InitWorkDay: FC = () => {
     return (
         <>
-            <div>Packages</div>;
+            <DropdownCard title={'title'} subtitle={'subtitle'}>
+                <li className='flex'>shipment 6</li>
+                <li className='flex'>shipment 7</li>
+                <li className='flex'>shipment 8</li>
+                <li className='flex'>shipment 9</li>
+                <li className='flex'>shipment 10</li>
+            </DropdownCard>
+            <GeneralCard title={'title'}>
+                <p className='text-gray-800 text-base'>TEST</p>
+                <p className='text-gray-800 text-base'>TEST</p>
+                <p className='text-gray-800 text-base'>TEST</p>
+                <p className='text-gray-800 text-base'>TEST</p>
+            </GeneralCard>
         </>
     );
 };
