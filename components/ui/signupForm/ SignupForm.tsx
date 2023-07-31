a1cd6c94159ea8e34b7026e4453fae3414bdc601
Jtendersen <jtendersen@hotmail.com>
2023-07-31 13:23:25 -0300

    add flow logic to login/signup

--- components/ui/signupForm/ SignupForm.tsx
+++ components/ui/signupForm/ SignupForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import MainButton from '../../commons/buttons/MainButton';
 import {
     RiUserLine,
@@ -8,16 +8,63 @@ import {
     RiEyeFill,
     RiEyeOffFill,
 } from 'react-icons/ri';
+import { useForm } from '../../../hooks/useForm';
 
-export const SignupForm = () => {
+import Link from 'next/link';
+
+// type SignUpParams = {
+//     email: string;
+//     password: string;
+//     username: string;
+// };
+
+// const signUp = async (params: SignUpParams) => {
+//     try {
+//         const user = await Auth.signUp(params);
+//         if (user.userConfirmed) {
+//             window.location.href = '/';
+//         } else {
+//             window.location.href = '/confirm';
+//         }
+//         console.log(user);
+//     } catch (error) {
+//         console.log(error);
+//         alert(error);
+//         window.location.href = '/';
+//     }
+// };
+
+export const SignupForm: FC = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    const { values, handleChange } = useForm({
+        email: '',
+        password: '',
+        repeatPassword: '',
+    });
+
+    const handleSubmit = async (event: FormEvent) => {
+        const { email, password } = values;
+        event.preventDefault();
+        console.log(values);
+        console.log(email);
+        console.log(password);
+        setIsAuthenticated(true);
+    };
 
     return (
         <>
+            {isAuthenticated && <div>User Authenticated</div>}
             <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
                 <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-                    <form className='space-y-6' action='#' method='POST'>
+                    <form
+                        className='space-y-6'
+                        action='#'
+                        method='POST'
+                        onSubmit={handleSubmit}
+                    >
                         <div>
                             <div className='relative mt-2'>
                                 <input
@@ -25,8 +72,10 @@ export const SignupForm = () => {
                                     id='email'
                                     name='email'
                                     type='email'
+                                    value={values.email}
                                     autoComplete='email'
                                     required
+                                    onChange={handleChange}
                                     className='block w-full rounded-lg border-1 px-12 py-3.5 text-primary shadow-sm ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 bg-transparent'
                                 />
                                 <span className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
@@ -41,9 +90,11 @@ export const SignupForm = () => {
                                     id='password'
                                     name='password'
                                     type={showPassword ? 'text' : 'password'}
+                                    value={values.password}
                                     placeholder='YourUltraSecretPassword'
                                     autoComplete='current-password'
                                     required
+                                    onChange={handleChange}
                                     className='block w-full rounded-lg border-1 px-12 py-3.5 text-primary shadow-sm ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 bg-transparent'
                                 />
                                 <span className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
@@ -66,14 +117,16 @@ export const SignupForm = () => {
                         <div>
                             <div className='relative mt-2'>
                                 <input
-                                    id='password'
-                                    name='password'
+                                    id='repeatPassword'
+                                    name='repeatPassword'
                                     type={
                                         showRepeatPassword ? 'text' : 'password'
                                     }
+                                    value={values.repeatPassword}
                                     placeholder='YourUltraSecretPassword'
                                     autoComplete='current-password'
                                     required
+                                    onChange={handleChange}
                                     className='block w-full rounded-lg border-1 px-12 py-3.5 text-primary shadow-sm ring-1 ring-inset ring-white placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 bg-transparent'
                                 />
                                 <span className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400'>
@@ -97,7 +150,12 @@ export const SignupForm = () => {
                         </div>
                         <div className='space-y-6'>
                             <div className='mt-20 '>
-                                <MainButton text='Create Account' btnGreen />
+                                <Link href='/confirm'>
+                                    <MainButton
+                                        text='Create Account'
+                                        btnGreen
+                                    />
+                                </Link>
                             </div>
                             <div>
                                 <div className='flex items-center justify-center'>
@@ -107,10 +165,12 @@ export const SignupForm = () => {
                                         </p>
                                     </div>
                                 </div>
-                                <MainButton text='Sign In' btnBlue />
                             </div>
                         </div>
                     </form>
+                    <Link href='/'>
+                        <MainButton text='Sign In' btnBlue />
+                    </Link>
                 </div>
             </div>
         </>
