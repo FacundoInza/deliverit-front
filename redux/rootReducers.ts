3ff1ac3457218ac4b1ffaa6bb38bbda17db9d7eb
Facundo Inza <facundoinza5@gmail.com>
2023-09-22 11:15:06 -0400

    feat: Global state user with redux and actions slices

--- redux/rootReducers.ts
+++ redux/rootReducers.ts
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import packagesReducer from '../redux/features/packages/packagesSlice';
 import deliveriesReducer from '../redux/features/deliveries/deliveriesSlice';
+import userReducer from './features/user/userSlice';
 
 const rootReducer = combineReducers({
     packages: packagesReducer,
     deliveries: deliveriesReducer,
+    user: userReducer,
 });
 
 export default rootReducer;
